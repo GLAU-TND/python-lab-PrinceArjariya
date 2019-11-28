3d65ce16c7d28d5dcfb27d536a0d4b4ff8af63ad
PrinceArjariya <46559176+PrinceArjariya@users.noreply.github.com>
2019-11-28 16:19:50 +0530

    Update customexception.py

--- customexception.py
+++ customexception.py
@@ -1,1 +1,16 @@
-class 
+class MyException(Exception):
+    def __init__(self ,v):
+        self.v=v
+
+
+def xyz(a,b):
+    c = a+b
+    if c<150:
+        raise TypeError('Custom Exception occurred')
+    else:
+        return c
+
+
+a = int(input())
+b = int(input())
+print(xyz(a,b))
