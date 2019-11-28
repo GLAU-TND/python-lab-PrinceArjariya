8f2a282945a51ff6082791aa02963a2333d39315
PrinceArjariya <46559176+PrinceArjariya@users.noreply.github.com>
2019-11-28 16:56:50 +0530

    question 2 answere

---
+++ exceptionhandlingerror.py
@@ -0,0 +1,23 @@
+def exceptionhandling():
+    a = 10
+    try:
+
+        a+='string'
+
+    except Exception as e:
+        print(type(e),__name__,e)
+
+    try:
+        a.abcd()
+    except Exception as e:
+        print(type(e),__name__,e)
+
+
+    try:
+        int('string')
+    except Exception as e:
+       print(type(e),__name__,e)
+
+
+if __name__=="__main__":
+    exceptionhandling()
